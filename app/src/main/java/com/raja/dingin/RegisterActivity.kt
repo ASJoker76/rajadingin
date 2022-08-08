464282a84b1930044615c802c5549ee7b3df42ba
12205002 <agus_setiawan@aeon.ac.id>
2022-08-08 16:13:01 +0700

    update spinner

--- app/src/main/java/com/raja/dingin/RegisterActivity.kt
+++ app/src/main/java/com/raja/dingin/RegisterActivity.kt
@@ -4,36 +4,38 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.view.get
 import cn.pedant.SweetAlert.SweetAlertDialog
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.raja.dingin.connection.API
 import com.raja.dingin.model.prov.*
 import com.raja.dingin.model.req.ReqRegister
 import com.raja.dingin.model.res.ResUtama
 import com.raja.dingin.utils.LocationFormatter
+import id.rizmaulana.sheenvalidator.lib.SheenValidator
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register.*
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 
 
 class RegisterActivity : AppCompatActivity(), LocationFormatter {
+
+    private lateinit var sheenValidator: SheenValidator
 
     private var fusedLocationClient: FusedLocationProviderClient? = null
     private var lastLocation: Location? = null
@@ -75,80 +77,23 @@ class RegisterActivity : AppCompatActivity(), LocationFormatter {
             })
 
         onChange()
-    }
 
-    private fun onChange() {
-
-        spProvinsi.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
-            callKabupaten()
-            spKabupaten.text = null
-            spKecamatan.text = null
-            spKelurahan.text = null
-        })
-
-        spKabupaten.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
-            callKecamatan()
-            spKecamatan.text = null
-            spKelurahan.text = null
-        })
-
-        spKecamatan.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
-            callKelurahan()
-            spKelurahan.text = null
-        })
-
-        btn_register.setOnClickListener {
-            if (tvAddress.text.toString().length == 0){
-                tvAddress.setError(R.string.can_not_be_empty.toString())
-            }
-            if (tvEmail.getText().toString().length == 0){
-                tvEmail.setError(R.string.can_not_be_empty.toString())
-            }
-//            if (spKabupaten.getText().toString().trim().length < 1){
-//                spKabupaten.setError(R.string.can_not_be_empty.toString())
-//                return@setOnClickListener
-//            }
-//            if (spKecamatan.getText().toString().trim().length < 1){
-//                spKecamatan.setError(R.string.can_not_be_empty.toString())
-//                return@setOnClickListener
-//            }
-//            if (spKelurahan.getText().toString().trim().length < 1){
-//                spKelurahan.setError(R.string.can_not_be_empty.toString())
-//                return@setOnClickListener
-//            }
-            if (tvName.getText().toString().length == 0){
-                tvName.setError(R.string.can_not_be_empty.toString())
-            }
-            if (tvPassword.getText().toString().length == 0){
-                tvPassword.setError(R.string.can_not_be_empty.toString())
-            }
-            if (tvPhoneNumber.getText().toString().length == 0){
-                tvPhoneNumber.setError(R.string.can_not_be_empty.toString())
-            }
-            if (tvPostalCode.getText().toString().length == 0){
-                tvPostalCode.setError(R.string.can_not_be_empty.toString())
-            }
-//            if (spProvinsi.getText().toString().trim().length < 1){
-//                spProvinsi.setError(R.string.can_not_be_empty.toString())
-//                return@setOnClickListener
-//            }
-            if (tvUsername.getText().toString().length == 0){
-                tvUsername.setError(R.string.can_not_be_empty.toString())
-            }
-            else {
+        sheenValidator = SheenValidator(this)
+        sheenValidator.setOnValidatorListener {
+            if(!tvPasswordConfirm.equals(tvPassword)){
                 var register = ReqRegister(
                     tvAddress.text.toString(),
                     tvEmail.text.toString(),
-                    spKabupaten.text.toString(),
-                    spKecamatan.text.toString(),
-                    spKelurahan.text.toString(),
+                    tvKabupaten.text.toString(),
+                    tvKecamatan.text.toString(),
+                    tvKelurahan.text.toString(),
                     latitudeLabel.toString(),
                     longitudeLabel.toString(),
                     tvName.text.toString(),
                     tvPassword.text.toString(),
                     tvPhoneNumber.text.toString(),
                     tvPostalCode.text.toString(),
-                    spProvinsi.text.toString(),
+                    tvProvinsi.text.toString(),
                     tvUsername.text.toString()
                 )
 
@@ -173,6 +118,102 @@ class RegisterActivity : AppCompatActivity(), LocationFormatter {
                         }
                     })
             }
+        }
+        sheenValidator.registerAsRequired(tvEmail)
+        sheenValidator.registerAsEmail(tvEmail)
+        sheenValidator.registerAsRequired(tvName)
+        sheenValidator.registerAsRequired(tvUsername)
+        sheenValidator.registerAsPhone(tvPhoneNumber)
+        sheenValidator.registerAsRequired(tvAddress)
+        sheenValidator.registerAsRequired(tvProvinsi)
+        sheenValidator.registerAsRequired(tvPostalCode)
+        sheenValidator.registerAsRequired(tvPhoneNumber)
+        sheenValidator.registerAsRequired(tvPasswordConfirm)
+    }
+
+    private fun onChange() {
+
+        tvProvinsi.setOnClickListener{
+            spProvinsi.performClick()
+        }
+
+        spProvinsi.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                tvProvinsi.setText(spProvinsi.selectedItem.toString())
+                callKabupaten()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+
+        tvKabupaten.setOnClickListener{
+            spKabupaten.performClick()
+        }
+
+        spKabupaten.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                tvKabupaten.setText(spKabupaten.selectedItem.toString())
+                callKecamatan()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+
+        tvKecamatan.setOnClickListener{
+            spKecamatan.performClick()
+        }
+
+        spKecamatan.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                tvKecamatan.setText(spKecamatan.selectedItem.toString())
+                callKelurahan()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+
+        tvKelurahan.setOnClickListener{
+            spKelurahan.performClick()
+        }
+
+        spKelurahan.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                tvKelurahan.setText(spKelurahan.selectedItem.toString())
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        })
+
+        btn_register.setOnClickListener {
+            sheenValidator.validate()
         }
     }
 
@@ -200,8 +241,8 @@ class RegisterActivity : AppCompatActivity(), LocationFormatter {
 
     private fun callKabupaten() {
         var provinsi = Provinsi(
-            //spProvinsi.getSelectedItem().toString()
-            spProvinsi.text.toString()
+            spProvinsi.getSelectedItem().toString()
+//            spProvinsi.text.toString()
         )
 
         API.buildService().getKabupaten(provinsi)
@@ -230,9 +271,8 @@ class RegisterActivity : AppCompatActivity(), LocationFormatter {
 
     private fun callKecamatan() {
         var kabupaten = ReqKabupaten(
-            //spProvinsi.getSelectedItem().toString(),
-            spProvinsi.text.toString(),
-            spKabupaten.text.toString(),
+            spProvinsi.getSelectedItem().toString(),
+            spKabupaten.getSelectedItem().toString(),
         )
 
         API.buildService().getKecamatan(kabupaten)
@@ -261,10 +301,9 @@ class RegisterActivity : AppCompatActivity(), LocationFormatter {
 
     private fun callKelurahan() {
         var kecamatan = ReqKecamatan(
-            //spProvinsi.getSelectedItem().toString(),
-            spProvinsi.text.toString(),
-            spKabupaten.text.toString(),
-            spKecamatan.text.toString()
+            spProvinsi.getSelectedItem().toString(),
+            spKabupaten.getSelectedItem().toString(),
+            spKecamatan.getSelectedItem().toString()
         )
 
         API.buildService().getKelurahan(kecamatan)
